737e314d0a991188829006854b00e396016c09c9
Eliza Hynek <eliza.hynek@gmail.com>
2022-11-26 19:05:00 +0100

    add init files

---
+++ project-pizzeria 2/src/js/functions.js
@@ -0,0 +1,41 @@
+/* global Handlebars */
+
+const utils = {}; // eslint-disable-line no-unused-vars
+
+utils.createDOMFromHTML = function(htmlString) {
+  let div = document.createElement('div');
+  div.innerHTML = htmlString.trim();
+  return div.firstChild;
+};
+
+utils.createPropIfUndefined = function(obj, key, value = []){
+  if(!obj.hasOwnProperty(key)){
+    obj[key] = value;
+  }
+};
+
+utils.serializeFormToObject = function(form){
+  let output = {};
+  if (typeof form == 'object' && form.nodeName == 'FORM') {
+    for (let field of form.elements) {
+      if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
+        if (field.type == 'select-multiple') {
+          for (let option of field.options) {
+            if(option.selected) {
+              utils.createPropIfUndefined(output, field.name);
+              output[field.name].push(option.value);
+            }
+          }
+        } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+          utils.createPropIfUndefined(output, field.name);
+          output[field.name].push(field.value);
+        } else if(!output[field.name]) output[field.name] = [];
+      }
+    }
+  }
+  return output;
+};
+
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+  return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
---
+++ project-pizzeria 2/src/js/script.js
@@ -0,0 +1,67 @@
+/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+
+{
+  'use strict';
+
+  const select = {
+    templateOf: {
+      menuProduct: '#template-menu-product',
+    },
+    containerOf: {
+      menu: '#product-list',
+      cart: '#cart',
+    },
+    all: {
+      menuProducts: '#product-list > .product',
+      menuProductsActive: '#product-list > .product.active',
+      formInputs: 'input, select',
+    },
+    menuProduct: {
+      clickable: '.product__header',
+      form: '.product__order',
+      priceElem: '.product__total-price .price',
+      imageWrapper: '.product__images',
+      amountWidget: '.widget-amount',
+      cartButton: '[href="#add-to-cart"]',
+    },
+    widgets: {
+      amount: {
+        input: 'input[name="amount"]',
+        linkDecrease: 'a[href="#less"]',
+        linkIncrease: 'a[href="#more"]',
+      },
+    },
+  };
+
+  const classNames = {
+    menuProduct: {
+      wrapperActive: 'active',
+      imageVisible: 'active',
+    },
+  };
+
+  const settings = {
+    amountWidget: {
+      defaultValue: 1,
+      defaultMin: 1,
+      defaultMax: 9,
+    }
+  };
+
+  const templates = {
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  };
+
+  const app = {
+    init: function(){
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+    },
+  };
+
+  app.init();
+}

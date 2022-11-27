6f008b8b83160311b92ca534831fca5d132e6612
Eliza Hynek <eliza.hynek@gmail.com>
2022-11-27 17:26:00 +0100

    correct initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -97,7 +97,7 @@
         const activeProduct = document.querySelector(select.all.menuProductsActive);
 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if (activeProduct && activeProduct != thisProduct.element){
+        if (activeProduct !== null && activeProduct != thisProduct.element){
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
         }
 

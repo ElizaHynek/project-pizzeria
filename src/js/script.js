891e37e0b47388677abc63af61685150d5bbfb6d
Eliza Hynek <eliza.hynek@gmail.com>
2022-12-07 19:07:50 +0100

    add correct widget amount

--- src/js/script.js
+++ src/js/script.js
@@ -272,7 +272,7 @@
       const thisWidget = this;
 
       thisWidget.getElements(element);
-      thisWidget.setValue(settings.amountWidget.defaultValue);
+      thisWidget.setValue(thisWidget.input.value || settings.amountWidget.defaultValue);
       thisWidget.initActions();
     }
 

0d3817afff65a39d37945eee462e1d502a454a40
Eliza Hynek <eliza.hynek@gmail.com>
2022-12-13 14:23:38 +0100

    add class Booking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function(){
@@ -63,6 +64,14 @@ const app = {
 
   },
 
+  initBooking: function(){
+    const thisApp = this;
+
+    thisApp.bookingCntr = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(thisApp.bookingCntr);
+
+  },
+
   initMenu: function(){
     const thisApp = this;
 
@@ -115,6 +124,7 @@ const app = {
 
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
---
+++ src/js/components/Booking.js
@@ -0,0 +1,41 @@
+import { templates, select } from '../settings.js';
+import utils from '../utils.js';
+import AmountWidget from './AmountWidget.js';
+
+class Booking {
+  constructor(element){
+    const thisBooking = this;
+
+    thisBooking.render(element);
+    thisBooking.initWidgets();
+  }
+
+  render(element){
+    const thisBooking = this;
+
+    const generatedHTML = templates.bookingWidget();
+    thisBooking.element = utils.createDOMFromHTML(generatedHTML);
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    bookingContainer.appendChild(thisBooking.element).innerHTML;
+
+    thisBooking.dom = {};
+    thisBooking.dom.wrapper = element;
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+  }
+
+  initWidgets(){
+    const thisBooking = this;
+
+    thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.dom.peopleAmount.addEventListener('updated', function(){
+      thisBooking.amount = thisBooking.amountWidget;
+    });
+    thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('updated', function(){
+      thisBooking.amount = thisBooking.amountWidget;
+    });
+  }
+}
+
+export default Booking;

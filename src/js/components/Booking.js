31a58225befb2418c47ec279536c0ab1aafd3227
Eliza Hynek <eliza.hynek@gmail.com>
2022-12-16 12:23:07 +0100

    add download data from API

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import { templates, select } from '../settings.js';
+import { templates, select, settings } from '../settings.js';
 import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
@@ -10,6 +10,68 @@ class Booking {
 
     thisBooking.render(element);
     thisBooking.initWidgets();
+    thisBooking.getData();
+
+  }
+
+  getData(){
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+
+    //console.log('getData params', params);
+
+    const urls = {
+      booking:       settings.db.url + '/' +
+                     settings.db.bookings + '?' + 
+                     params.booking.join('&'),
+      eventsCurrent: settings.db.url + '/' + 
+                     settings.db.events + '?' + 
+                     params.eventsCurrent.join('&'),
+      eventsRepeat:  settings.db.url + '/' + 
+                     settings.db.events + '?' + 
+                     params.eventsRepeat.join('&'),
+    };
+
+    //console.log('getData urls', urls);
+
+    Promise.all([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log('bookings', bookings);  // uwaga! nie wyświetlają się dane w tablicy
+        console.log('eventsCurrent', eventsCurrent);
+        console.log('eventsRepeat', eventsRepeat);
+      });
   }
 
   render(element){

92234d6ec124324123530bc79086e86ac42111f4
Eliza Hynek <eliza.hynek@gmail.com>
2022-12-29 11:03:45 +0100

    add files for replit

--- src/js/settings.js
+++ src/js/settings.js
@@ -121,7 +121,7 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     bookings: 'bookings',
